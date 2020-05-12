ef378a06200355c4bfb680a464316db2e6f62409
Kévin Le Devéhat <kevin.ledevehat2018@campus-eni.fr>
2020-05-12 15:03:34 +0200

    TP1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  options = {easy: 'Easy', medium: 'Medium', hard: 'hard'};
+  optionsOrder = ['easy', 'medium', 'hard'];
+  name = '';
+  mode = 'easy';
+  save = false;
+  error = '';
+  startedGame = false;
+  answers = {a: 'Réponse A', b: 'Réponse B', c: 'Réponse C', d: 'Réponse D'};
+  answersOrder = ['a', 'b', 'c', 'd'];
+  selectedAnswer: string = null;
+
   constructor() {}
 
+  start() {
+    if (!this.name || this.name.length < 3) {
+      this.error = 'Le pseudo doit faire au moins 3 caractères.';
+      return;
+    }
+    if (!this.mode || this.mode.length === 0) {
+      this.error = 'Veuillez choisir une difficulté.'
+    }
+    this.startedGame = true;
+  }
+
+  selectAnswer(anwser: string) {
+    this.selectedAnswer = anwser;
+  }
 }
